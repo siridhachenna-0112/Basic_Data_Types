66f051628844e331176427942d943e7b64919dae
siridhachenna-0112 <chennasiridha5002@gmail.com>
2025-01-24 14:56:51 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,16 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int a=4;
+        char b='s';
+        String c="siri";
+        float d=10.5f;
+        double e=10.3;
+        Boolean f=true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
